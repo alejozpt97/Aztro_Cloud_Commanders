d4c9608efc84384c3cc745453ec4c600089df813
Alejandro Zapata <alejozpt97@gmail.com>
2025-07-03 21:42:19 -0500

    first commit

---
+++ shared-vars.sh
@@ -0,0 +1,12 @@
+# shared-vars.sh
+
+# Sufijo compartido para nombres de recursos
+SUFFIX="alt-a"
+
+# Ubicación común para los recursos
+LOCATION="centralus"
+
+# Docker images and user
+API_IMAGE="aztro-api"
+WEB_IMAGE="aztro-web"
+DOCKERHUB_USER="japersa" # Change this to your Docker Hub username
